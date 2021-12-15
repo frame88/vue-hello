ec269a04f5957e518f71510560e391d9f7aea3e7
frame88 <fleuzzi97@gmail.com>
2021-12-15 17:03:59 +0100

    1 obbiettivo completato

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+*/
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
