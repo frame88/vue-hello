8fe6481032ee2726548332b6ab1ca90106d169be
frame88 <fleuzzi97@gmail.com>
2021-12-19 14:20:12 +0100

    bonus start

---
+++ bonus/bonus.js
@@ -0,0 +1,28 @@
+/*
+Bonus:
+In una cartella bonus:
+Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
+Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+Poi aggiungiamo un pulsante per aumentare questo numero.
+: githubparrot:
+11
+*/
+
+var app = new Vue ({
+    el: '#app',
+    data: prompt('inserisci del testo : ')
+}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
